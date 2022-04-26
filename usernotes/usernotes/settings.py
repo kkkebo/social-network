2f6c31811a8b0aceb020a71c560e65575e881d84
Andrey.K <andrey-8991@yandex.ru>
2022-04-26 17:37:54 +0300

    added register with bad frontend

--- usernotes/usernotes/settings.py
+++ usernotes/usernotes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'posts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
--- usernotes/usernotes/urls.py
+++ usernotes/usernotes/urls.py
@@ -18,8 +18,16 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # импорт правил из приложения posts
+    #  обработчик для главной страницы ищем в urls.py приложения posts
     path("", include("posts.urls")),
-    # импорт правил из приложения admin
+
+    #  регистрация и авторизация
+    path("auth/", include("users.urls")),
+
+    #  если нужного шаблона для /auth не нашлось в файле users.urls —
+    #  ищем совпадения в файле django.contrib.auth.urls
+    path("auth/", include("django.contrib.auth.urls")),
+
+    #  раздел администратора
     path("admin/", admin.site.urls),
 ]
