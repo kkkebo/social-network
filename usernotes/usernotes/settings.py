3b6285a5ccdfdfd5d39480bc5b75f80402f13354
Andrey.K <andrey-8991@yandex.ru>
2022-04-28 16:43:26 +0300

    Added Login and Recovery Page Templates. Added Mail server emulation

--- usernotes/usernotes/settings.py
+++ usernotes/usernotes/settings.py
@@ -126,3 +126,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index"
+
+# LOGOUT_REDIRECT_URL = "index"
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
